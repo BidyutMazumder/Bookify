183ba5974e3892d5e4f3bd32e45ac411b7061e12
Hriday majumder <bid.mder31@gmail.com>
2025-03-08 21:41:39 +0600

    12

--- Bookify.Domain/Apartments/Apartment.cs
+++ Bookify.Domain/Apartments/Apartment.cs
@@ -1,17 +1,17 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using Bookify.Domain.Abstraction;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Bookify.Domain.Apartments;
 
-public sealed class Apartment
+public sealed class Apartment: Entity
 {
-    public Guid Id { get; private set; }
+    public Apartment(Guid id) : base(id)
+    {
+    }
+
     public string Name { get; private set; }
     public string Description { get; private set; }
-    public string Country { get; private set; }
-    public string State { get; private set; }
-    public string ZipCode { get; private set; }
-    public string City { get; private set; }
-    public string Street { get; private set; }
+    public Address Address { get; private set; }
     public decimal PriceAmount { get; private set; }
     public string PriceCurrency { get; private set; }
     public decimal CleeningFeeAmount { get; private set; }
