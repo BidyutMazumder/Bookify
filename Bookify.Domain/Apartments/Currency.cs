d7bc6a78c7676c4a36592e7ad2367026a5d9ca38
Hriday majumder <bid.mder31@gmail.com>
2025-03-14 23:41:47 +0600

    13

---
+++ Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,19 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Currency
+{
+    private static readonly Currency Usd = new("USD");
+    private static readonly Currency Eru = new("ERU");
+    private Currency(string code) {
+        Code = code;
+    }
+    public string Code {  get; init; }
+
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(c => c.Code == code) ??
+            throw new ApplicationException("The Currency Code Is Invalied");
+    }
+    public static readonly IReadOnlyCollection<Currency>All = new[]
+    { Usd, Eru };
+}
