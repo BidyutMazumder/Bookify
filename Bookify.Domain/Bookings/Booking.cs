13498f7385fdd3df3ef1205769a1d6e278994170
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2025-03-18 00:21:40 +0600

    update

---
+++ Bookify.Domain/Bookings/Booking.cs
@@ -0,0 +1,25 @@
+﻿using Bookify.Domain.Abstraction;
+using Bookify.Domain.Apartments;
+
+namespace Bookify.Domain.Bookings;
+
+public sealed class Booking : Entity
+{
+    public Booking(Guid id) : base(id)
+    {
+    }
+    public Guid AppertmentId { get; private set; }
+    public Guid UserId { get; private set; }
+    public DateRange DateRange { get; private set; }
+    public Money PriceForPeriod { get; private set; }
+    public Money CleaningFee { get; private set; }
+    public Money AmenitiesUpcharge { get; private set; }
+    public Money TotalPrice { get; private set; }
+    public BookingStatus Status { get; private set; }
+    public DateTime? CreatedOnUtc { get; private set; }
+    public DateTime? ConfirmedOnUtc { get; private set; }
+    public DateTime? RejectedOnUtc { get; private set; }
+    public DateTime? CompletedOnUtc { get; private set; }
+    public DateTime? CancelledOnUtc { get; private set; }
+
+}
