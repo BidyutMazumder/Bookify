8b17c189320b73b7b1d733439c90fe208fecbb5d
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2025-04-20 17:52:01 +0600

    28 running

---
+++ Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommand.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using MediatR;
+
+namespace Bookify.Application.Bookings.ReserveBooking;
+
+public record ReserveBookingCommand
+    (
+        Guid AppartmentId,
+        Guid UserId,
+        DateTime StartDate,
+        DateTime EndDate
+    ): ICommand<Guid>;
---
+++ Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
+
+namespace Bookify.Application.Bookings.ReserveBooking;
+
+internal sealed class ReserveBookingCommandHandler : ICommandHandler<ReserveBookingCommand, Guid>
+{
+    public Task<Result<Guid>> Handle(ReserveBookingCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
