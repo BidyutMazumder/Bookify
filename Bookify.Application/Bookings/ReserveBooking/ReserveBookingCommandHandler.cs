c653b885e90020ebb3c39b7d3b86dcf1f89a96d8
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2025-04-21 23:42:47 +0600

    update

--- Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
+++ Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
@@ -1,10 +1,18 @@
 ﻿using Bookify.Application.Abstractions.Messaging;
 using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
 
 namespace Bookify.Application.Bookings.ReserveBooking;
 
 internal sealed class ReserveBookingCommandHandler : ICommandHandler<ReserveBookingCommand, Guid>
 {
+    private readonly IUserRepository _userRepository;
+    private readonly IApartmentRepository _apartmentRepository;
+    private readonly IBookingRepository _bookingRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly PricingService _pricingService;
+    private readonly IDateTimeProvider _dateTimeProvider;
     public Task<Result<Guid>> Handle(ReserveBookingCommand request, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
