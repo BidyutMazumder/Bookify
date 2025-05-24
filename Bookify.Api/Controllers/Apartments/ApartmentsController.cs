ab1ce91243149dca86e3f9f5b88c4ed85980e3c5
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2025-05-24 17:40:41 +0600

    49

---
+++ Bookify.Api/Controllers/Apartments/ApartmentsController.cs
@@ -0,0 +1,33 @@
+﻿using Bookify.Application.Apartments.SearchApartments;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Api.Controllers.Apartments
+{
+    [Route("api/apartments")]
+    [ApiController]
+    public class ApartmentsController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public ApartmentsController(ISender sender)
+        {
+            this._sender = sender;
+        }
+        [HttpGet]
+        public async Task<IActionResult> SearchAppartments
+            (
+            DateOnly StartDate,
+            DateOnly EndDate,
+            CancellationToken cancellationToken
+            )
+        {
+            var Query = new SearchApartmentsQuery(StartDate, EndDate);
+
+            var result = await _sender.Send(Query, cancellationToken);
+          
+            return Ok(result.Value);
+        }
+    }
+}
---
+++ Bookify.Api/ServiceRegestration/ServiceConfigurationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Bookify.Application;
+using Bookify.Infrastructure;
+
+namespace Bookify.Api.ServiceRegestration;
+
+public static class ServiceConfigurationExtensions
+{
+    public static void RegisterAllServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Register MediatR services
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+        });
+        services.AddApplication();
+        services.AddInfrastructure(configuration); 
+    }
+}
