e6d87d010e788a1999695b2d07bec8c2587bca70
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2025-05-27 12:26:30 +0600

    Remove local DB data from Git tracking

--- Bookify.Api/Program.cs
+++ Bookify.Api/Program.cs
@@ -1,3 +1,4 @@
+using Bookify.Api.Extension;
 using Bookify.Api.ServiceRegestration;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +22,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
+    //app.ApplyMigrations();
+    //app.SeedData();
 }
 app.MapHealthChecks("/health");
 app.UseHttpsRedirection();
